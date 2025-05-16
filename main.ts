18a9a1913eb1766a9eb0c4edfce096d909ceb477
Alexandre Chataignon <alexandre.git@xouillet.info>
2025-05-17 00:26:56 +0200

    Scale playground

--- main.ts
+++ main.ts
@@ -3,13 +3,13 @@ import VectorLayer from "ol/layer/Vector.js";
 import VectorSource from "ol/source/Vector.js";
 import View from "ol/View.js";
 import { State } from "ol/render";
-import { Projection } from "ol/proj";
+import { addCoordinateTransforms, addProjection, Projection } from "ol/proj";
 import ImageLayer from "ol/layer/Image";
 import Static from "ol/source/ImageStatic.js";
 import Feature from "ol/Feature";
 import Select, { SelectEvent } from "ol/interaction/Select.js";
-import { Coordinate } from "ol/coordinate";
-import { getWidth, getHeight, getBottomLeft } from "ol/extent";
+import { add, Coordinate, rotate } from "ol/coordinate";
+import { getWidth, getHeight, getBottomLeft, getCenter } from "ol/extent";
 import { Polygon } from "ol/geom";
 import Style from "ol/style/Style";
 
@@ -19,116 +19,61 @@ export interface Pose {
   t: number;
 }
 
-const _extent = [0, 0, 14.25, 14.25];
-const _projection = new Projection({
-  code: "Custom",
-  units: "m",
-  extent: _extent,
-});
+const scale = (x: number) => (x * 5) / 100;
 
-const _map = new Map({
+const map_extent = [0, 0, 773, 709].map(scale);
+const map_projection = new Projection({
+  code: "Robocc",
+  units: "m",
+  extent: map_extent,
+});
+addProjection(map_projection);
+
+const all = new Map({
   target: "map",
   layers: [],
   view: new View({
-    projection: _projection,
-    center: [7, 7],
+    projection: map_projection,
+    center: getCenter(map_extent),
     zoom: 2,
     maxZoom: 8,
   }),
 });
 
-const background = new ImageLayer({
+const map = new ImageLayer({
   source: new Static({
-    url: "./map.webp",
-    projection: _projection,
-    imageExtent: _extent,
+    url: "./current_map-2.webp",
+    projection: map_projection,
+    imageExtent: map_extent,
   }),
 });
-_map.addLayer(background);
+all.addLayer(map);
 
-function rectangle(pose: Pose) {
-  const robot_width = 0.426;
-  const robot_length = 0.67;
-  const polygon = new Polygon([
-    [
-      [pose.x - robot_width / 2, pose.y - robot_length / 2],
-      [pose.x + robot_width / 2, pose.y - robot_length / 2],
-      [pose.x + robot_width / 2, pose.y + robot_length / 2],
-      [pose.x - robot_width / 2, pose.y + robot_length / 2],
-    ],
-  ]);
+const [angle, dx, dy] = [-2.632765769958496, 678, 1104];
 
-  return polygon;
-}
-
-function featureRenderer(
-  pixelCoords: Coordinate | Coordinate[] | Coordinate[][],
-  state: State,
-  selected: boolean
-) {
-  pixelCoords = pixelCoords as Coordinate[][];
-
-  const context = state.context;
-  const geometry = state.geometry.clone() as Polygon;
-  geometry.setCoordinates(pixelCoords);
-  const extent = geometry.getExtent();
-  const width = getWidth(extent);
-  const height = getHeight(extent);
-  const bottomLeft = getBottomLeft(extent);
-  const left = bottomLeft[0];
-  const bottom = bottomLeft[1];
-
-  const svgwidth = 426;
-  const svgheight = 670;
-  const img = new Image();
-  img.src = `data:image/svg+xml;charset=utf-8,${encodeURIComponent(
-    `
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${svgwidth} ${svgheight}" height="${svgheight}px" width="${svgwidth}px">
-    <path 
-    fill="${selected ? "#800" : "#333"}" 
-      stroke="none" 
-      stroke-linecap="round" 
-      stroke-linejoin="round" 
-      stroke-miterlimit="10" 
-      d="M8.529,653.532 L129.262,670.5h164.203l120.733-16.968v-0.016c0,0,6.335,
-      0.043,8.764-2.002c2.325-1.959,2.249-7.496,2.249-7.496V28.46
-        c0,0,0.527-6.324-1.519-8.753c-1.958-2.326-7.495-2.249-7.495-2.249c0,
-        0-120.733-16.958-120.733-16.958H131.261L10.528,17.458
-        c0,0-5.086,0.212-7.515,2.258c-2.326,1.959-2.498,8.744-2.498,8.744v615.559c0,
-        0-0.297,4.067,1.749,6.496 C4.223,652.84,8.529,653.532,8.529,653.532z
-    "/>
-    <line x1="30" y1="50" x2="150" y2="30" stroke="white" stroke-width="20" stroke-linecap="round"></line>
-    <line x1="276" y1="30" x2="396" y2="45" stroke="white" stroke-width="20" stroke-linecap="round"></line>
-    <line x1="30" y1="625" x2="150" y2="640" stroke="red" stroke-width="20" stroke-linecap="round"></line>
-    <line x1="276" y1="640" x2="396" y2="625" stroke="red" stroke-width="20" stroke-linecap="round"></line>
-    </svg>
-    `
-  )}`;
-  context.drawImage(img, left, bottom, width, height);
-}
-
-let pose = { x: 7, y: 6.6, t: 0.5 };
-const robotFeature = new Feature(rectangle(pose));
-const robotStyle = new Style({
-  renderer: (
-    pixelCoords: Coordinate | Coordinate[] | Coordinate[][],
-    state: State
-  ) => featureRenderer(pixelCoords, state, false),
+const remap_extent = [0, 0, 1041, 1539].map(scale);
+const remap_projection = new Projection({
+  code: "RoboccRemap",
+  units: "m",
+  extent: map_extent,
 });
-const robotStyleSel = new Style({
-  renderer: (
-    pixelCoords: Coordinate | Coordinate[] | Coordinate[][],
-    state: State
-  ) => featureRenderer(pixelCoords, state, true),
-});
-robotFeature.setStyle(robotStyle);
+addCoordinateTransforms(
+  "Robocc",
+  remap_projection,
+  (c: Coordinate) => {
+    rotate(c, -angle)
+    add(c, [scale(dx), scale(dy)])
+    return c;
+  },
+  (p) => p
+);
 
-const robot_layer = new VectorLayer({
-  source: new VectorSource({ features: [robotFeature] }),
+const remap = new ImageLayer({
+  source: new Static({
+    url: "./current_remap-2.webp",
+    projection: remap_projection,
+    imageExtent: remap_extent,
+  }),
+  opacity: 0.7,
 });
-_map.addLayer(robot_layer);
-
-const select = new Select({
-  style: robotStyleSel,
-});
-_map.addInteraction(select);
+all.addLayer(remap);
